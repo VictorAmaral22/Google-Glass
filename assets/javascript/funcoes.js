1a9fb521b0e017262626ef49b64f848a7f15370f
victor-grandmasterwitcher <victortavamaral@gmail.com>
2021-05-28 02:30:18 -0300

    Fixes

--- assets/javascript/funcoes.js
+++ assets/javascript/funcoes.js
@@ -1,3 +1,7 @@
 function mudarIcone(a) {
-    document.getElementById("icone").src = "_imagens/" + a + ".png";
+    document.getElementById("icone").src = "../imagens/" + a + ".png";
+};
+
+function mudarIconeHome(a) {
+    document.getElementById("icone").src = "assets/imagens/" + a + ".png";
 };
